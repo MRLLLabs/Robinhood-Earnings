f0619648d2eb08c98694cb5974f84da17051d618
jonathanhkim <jhkim792@gmail.com>
2019-12-05 18:41:43 -0800

    styles the chart better

--- client/components/d3Create.js
+++ client/components/d3Create.js
@@ -3,85 +3,95 @@ import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 import * as d3 from 'd3';
 
+const ChartArea = styled.div`
+  padding: 10px 10px 74px 85px;
+`
 const chart = (data) => {
-  const height = 500;
-  const width = 750;
-  const margin = ({top: 20, right: 30, bottom: 30, left: 40})
+  const height = 120;
+  const width = 560;
+  // const margin = ({top: 20, right: 30, bottom: 30, left: 40})
+  const margin = ({top: 0, right: 0, bottom: 0, left: 0})
   const x = d3.scalePoint()
     .domain(data.map(d => d.x))
     .range([margin.left, width - margin.right])
   const y = d3.scaleLinear()
-      .domain(d3.extent(data, d => d.y)).nice()
+      .domain(d3.extent(data, d => d.y))
       .range([height - margin.bottom, margin.top])
   const xAxis = g => g
-    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .attr("transform", `translate(0,${height+ 45})`)
     .call(d3.axisBottom(x))
-    .call(g => g.select(".domain"))
+    .call(g => g.select(".domain").remove())
     .call(g => g.append("text")
         .attr("x", width - margin.right)
         .attr("y", -4)
-        .attr("font-weight", "bold")
         .attr("text-anchor", "end")
-        .text(data.x))
+        .attr("font-size", 13)
+        .attr("font-weight", 400)
+        .attr("line-height", "19px")
+        .text(data.name))
     .call(g => g.selectAll(".tick text")
-        .attr("stroke", "white"))
+        .attr("stroke", "#8c8c8e"))
     .call(g => g.selectAll(".tick line")
-        .attr("stroke", "white"))
+        .remove())
 
   const yAxis = g => g
-      .attr("transform", `translate(${margin.left},0)`)
+      .attr("transform", `translate(${-51},0)`)
       .call(d3.axisLeft(y))
-      .call(g => g.select(".domain"))
+      .call(g => g.select(".domain").remove())
       .call(g => g.select(".tick:last-of-type text").clone()
           .attr("x", 4)
           .attr("text-anchor", "start")
-          .attr("font-weight", "bold")
+          .attr("line-height", "19px")
+          .attr("font-family", "arial")
+          .attr("font-size", 13)
+          .attr("font-weight", 400)
           .text(data.y))
       .call(g => g.selectAll(".tick text")
-          .attr("stroke", "white"))
+          .attr("stroke", "#8c8c8e"))
       .call(g => g.selectAll(".tick line")
-          .attr("stroke", "white"))
+          .remove())
+
   const svg = d3.select('#earnings').append('svg')
-    .attr("viewBox", [0, 0, width, height]);
+    .attr("width", width)
+    .attr("height", height)
+    .attr("overflow", "visible")
 
   svg.append("g")
-      .call(xAxis);
-
+    .call(xAxis)
   svg.append("g")
-      .call(yAxis);
-
+    .call(yAxis)
   svg.append("g")
       .attr("stroke-width", 1.5)
       .attr("font-family", "arial")
-      .attr("font-size", 10)
+      .attr("font-size", 13)
+      .attr("font-weight", 400)
     .selectAll("g")
     .data(data)
     .join("g")
       .attr("transform", d => `translate(${x(d.x)},${y(d.y)})`)
+      .attr("opacity", d => {
+        return (d.type === "estimated" ? "20%" : "")
+      })
       .call(g => g.append("circle")
-          .attr("stroke", "#f45531")
           .attr("fill", "#f45531")
-          .attr("r", 3))
-      .call(g => g.append("text")
-          .attr("dy", "0.35em")
-          .attr('fill', 'white')
-          .attr("x", 7)
-          .text(d => d.name));
+          .attr("r", 7))
 }
 
 const formatData = (data) => {
   let formatted = data.map((data, idx) => {
     return {
-      name: data.date + " ESTIMATED",
-      x: idx,
-      y: data.estimated
+      name: data.date,
+      x: data.date,
+      y: data.estimated,
+      type: "estimated"
     }
   })
   data.forEach((data, idx) => {
     formatted.push({
-      name: data.date + " ACTUAL",
-      x: idx,
-      y: data.actual
+      name: data.date,
+      x: data.date,
+      y: data.actual,
+      type: "actual"
     })
   })
   return formatted
@@ -91,8 +101,9 @@ const D3Create = (props) => {
   let formatted = formatData(data)
   chart(formatted)
   return (
-    <div id="earnings">
-    </div>
+    <ChartArea>
+      <div id="earnings" />
+    </ChartArea>
   )
 }
 
