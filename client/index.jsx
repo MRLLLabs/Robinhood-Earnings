4211ad627337e8961978ce3abea7c376d8a95cd7
jonathanhkim <jhkim792@gmail.com>
2019-12-09 19:13:37 -0800

    changing ports and ids

--- client/index.jsx
+++ client/index.jsx
@@ -13,4 +13,4 @@ const App = () => {
   )
 }
 
-ReactDOM.render( <App /> , document.getElementById('earnings-graph'))
+ReactDOM.render( <App /> , document.getElementById('earnings'))
--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
 const app = require('./server.js')
-const port = 3000
+const port = 3006
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
