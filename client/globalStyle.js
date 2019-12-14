44e967c7b64acaf3fded79c3fd6bcf1662b72fb3
jonathanhkim <jhkim792@gmail.com>
2019-12-13 23:48:50 -0800

    trying this out

--- client/globalStyle.js
+++ client/globalStyle.js
@@ -1,16 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 import path from 'path';
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    font-family: "DINPro";
-    width: 750px;
-    margin: auto;
-    font-size: 13px;
-    font-weight: 400;
-    letter-spacing: 0.25px;
-    line-height: 19px;
-  }
-`
+const GlobalStyle = createGlobalStyle``
 
 export default GlobalStyle
--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const env = process.env.NODE_ENV;
 const host = env === "production" ? "database" : "localhost";
-const mongoUri = `mongodb://database/graph`;
+const mongoUri = `mongodb://localhost/graph`;
 
 const db = mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
 
